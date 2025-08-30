9b527115e8eb7479fa6efa1c0384f7de64571dfd
Inspector3ug <94785641+Inspector3ug@users.noreply.github.com>
2025-08-30 12:04:50 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwx6sDQ4mk1TXuQN45TYTZUjqj31Rk9Lk",
+  authDomain: "attendance-5a6e4.firebaseapp.com",
+  projectId: "attendance-5a6e4",
+  storageBucket: "attendance-5a6e4.firebasestorage.app",
+  messagingSenderId: "27858350207",
+  appId: "1:27858350207:web:1908c3cd46408fe090cf4a"
+};
